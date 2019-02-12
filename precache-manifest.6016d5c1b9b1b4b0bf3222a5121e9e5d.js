655678d77ceae9b65fa25c9ab40898315510371d
Aldrin P Vincent <aldrinpvincent@gmail.com>
2019-02-12 22:43:33 +0530

    Updates

---
+++ precache-manifest.6016d5c1b9b1b4b0bf3222a5121e9e5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fca560919cce6debde4",
+    "url": "/simple-task-manager/static/js/runtime~main.3fca5609.js"
+  },
+  {
+    "revision": "ccd7f15b27a845245492",
+    "url": "/simple-task-manager/static/js/main.ccd7f15b.chunk.js"
+  },
+  {
+    "revision": "892d80b21cfe918c90de",
+    "url": "/simple-task-manager/static/js/1.892d80b2.chunk.js"
+  },
+  {
+    "revision": "ccd7f15b27a845245492",
+    "url": "/simple-task-manager/static/css/main.ec921299.chunk.css"
+  },
+  {
+    "revision": "892d80b21cfe918c90de",
+    "url": "/simple-task-manager/static/css/1.05a27ae1.chunk.css"
+  },
+  {
+    "revision": "576ef6a142cdeaad5fb8e653fc81f7c8",
+    "url": "/simple-task-manager/index.html"
+  }
+];
